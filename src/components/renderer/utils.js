c5d1f21e983605f36ecf58ffb035dbc109729a30
Max Karpawich <max.karpawich@gmail.com>
2018-08-11 21:38:09 -0400

    "initial" commit

--- src/components/renderer/utils.js
+++
@@ -1,8 +0,0 @@
-function getFilenameFromPath(src) {
-  let split = src.split('/');
-  let splitLenght = split.length;
-  return split[splitLenght-1];
-}
-module.exports = {
-  getFilenameFromPath
-}
